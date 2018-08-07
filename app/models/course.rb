e38779f582ae5a0de731a1ff63d667fb20feb765
joalorro <joalorro@gmail.com>
2018-08-07 14:20:25 -0400

    Done.

---
+++ app/models/course.rb
@@ -0,0 +1,14 @@
+class Course
+  attr_reader :name,:topic
+  @@all = []
+
+  def initialize args
+    @name = args[:name]
+    @topic = args[:topic]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
